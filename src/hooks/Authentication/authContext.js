fc78295b02e5ddfe1e57bf72e5e9d291e5d56a1c
SupEliza <mercados.eliza@gmail.com>
2025-05-20 15:18:35 -0300

    diminuindo o tempo de auto refresh de 15 para 12 minutos para evitar do usuario nao autenticar

--- src/hooks/Authentication/authContext.js
+++ src/hooks/Authentication/authContext.js
@@ -77,7 +77,7 @@ export function AuthProvider({ children }) {
     useEffect(() => {
         const intervalId = setInterval(() => {
             refreshAccessToken();
-        }, 15 * 60 * 1000);
+        }, 12 * 60 * 1000);
 
         return () => {
             clearInterval(intervalId);
