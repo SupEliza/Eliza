3fb0e530d45deaf264c6e68888711d35b2cd6d2b
SupEliza <mercados.eliza@gmail.com>
2025-10-21 07:52:59 -0300

    mudando a forma que o arquivo txt de codes é gerado

--- src/pages/Dashboard/Sections/Codes/index.js
+++ src/pages/Dashboard/Sections/Codes/index.js
@@ -276,7 +276,7 @@ function Codes () {
 
     const exportList = response.codes.filter((code) => !code.isDeleted);
 
-    const txt = exportList.map((code, index) => `EAN ${index}: ${code.ean}\nQTD: ${code.quantity}`).join("\n\n");
+    const txt = exportList.map((code, index) => `EAN ${index + 1}: ${code.ean}\nQTD: ${code.quantity}`).join("\n\n");
     const blob = new Blob([txt], { type: "text/plain" }); 
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
